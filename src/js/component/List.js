23ff80b335dee59a7edc7b0d04ed290adbd90d8c
Jizga <jizga93@gmail.com>
2021-06-21 11:48:10 +0000

    Correct GET

--- src/js/component/List.js
+++ src/js/component/List.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Task } from "./Task";
 
 export function List() {
+	//Variables relacionadas con el fetch
 	const url = "https://assets.breatheco.de/apis/fake/todos/user/Vivi";
 	const options = {
 		method: "GET",
@@ -11,22 +12,107 @@ export function List() {
 		}
 	};
 
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	// ----------------------------------------------------------
+
 	const [list, setList] = useState([]);
+	const [inputTask, setInputTask] = useState("");
 
 	useEffect(() => {
 		fetch(url, options)
+			//Obtener los datos en formato JSON
 			.then(res => res.json())
-			.then(response => {
-				//Obtener la lista como un array de objetos (formato JSON)
-				setList(JSON.stringify(response));
-			})
+			.then(
+				result => {
+					setIsLoaded(true);
+					setList(result);
+				},
+				// Nota: es importante manejar errores aquí y no en
+				// un bloque catch() para que no interceptemos errores
+				// de errores reales en los componentes.
+				error => {
+					setIsLoaded(true);
+					setError(error);
+				}
+			)
 			.catch(error => console.error("Error:", error));
 	}, []);
 
+	console.log("LISTA : ", list, typeof list);
+
 	return (
-		<div>
-			{list}
-			<Task />
+		<div className="container text-center mt-5 mb-5 pt-3 pb-5 d-flex justify-content-center rounded myListContainer">
+			<div className="p-0 m-0 myContainer">
+				<div className="row d-flex justify-content-center">
+					<h1 className="col-9 col-sm-10 col-md-10 col-lg-12 col-xl-12 mb-3 mt-2">
+						TODO App
+					</h1>
+				</div>
+
+				<div className="row mb-4 d-flex justify-content-center">
+					<input
+						className="col-9 col-sm-10 col-md-10 col-lg-12 col-xl-12 border-0 rounded-pill text-center"
+						type="text"
+						onChange={e => setInputTask(e.target.value)}
+						value={inputTask}
+						// onKeyPress={e => pressEnter(e)}
+						placeholder="No tasks, add a task"
+						autoFocus
+					/>
+				</div>
+
+				<div className="row d-flex justify-content-center align-items-start">
+					<div className="d-flex flex-column col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+						<h4 className="col-8 col-sm-9 col-md-9 col-lg-12 col-xl-12 taskTitle">
+							To do tasks
+						</h4>
+						{list.map(task => {
+							return (
+								<Task
+									key={task.id}
+									id={task.id}
+									taskText={task.label}
+									done={task.done}
+									// addTaskDone={} deleteTask={} taskDoneList={} notDone={}
+								/>
+							);
+						})}
+
+						<div className="d-flex justify-content-start">
+							<div className="taskNum">
+								{list.length} tasks left
+							</div>
+						</div>
+					</div>
+
+					{/* <div className="d-flex flex-column col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                        <h4 className="col-8 col-sm-9 col-md-9 col-lg-12 col-xl-12 taskTitle">
+                            Done tasks
+						</h4>
+                        {taskDoneList.map(task => {
+                            return (
+                                <Task
+                                    key={task.id}
+                                    id={task.id}
+                                    taskText={task.text}
+                                    done={task.done}
+                                    deleteTask={deleteTask}
+                                    addTaskDone={addTaskDone}
+                                    taskDoneList={taskDoneList}
+                                    dontDone={dontDone}
+                                />
+                            );
+                        })}
+
+                        <div className="d-flex justify-content-start">
+                            <div className="taskNum">
+                                {taskDoneList.length} tasks done
+							</div>
+                        </div>
+                    </div> */}
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/js/component/Task.js
+++ src/js/component/Task.js
@@ -1,5 +1,74 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export function Task() {
-	return <div>Hola desde Task</div>;
+export function Task(props) {
+	return (
+		<div className="row no-gutters mt-2">
+			{props.done === false ? (
+				//Tarea sin hacer
+				<>
+					<div className="col-7 col-sm-8 col-md-8 col-lg-10 col-xl-10 myTask">
+						{props.taskText}
+						<hr></hr>
+					</div>
+
+					<div className="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+						<div className="d-flex pt-2">
+							<i
+								className="far fa-trash-alt mr-4"
+								// onClick={() =>
+								// 	props.deleteTask(props.id, null)
+								// }
+							></i>
+
+							<i
+								className="fas fa-check fa-lg"
+								// onClick={() => {
+								// 	props.addTaskDone(props.id);
+								// }}
+							></i>
+						</div>
+						<hr></hr>
+					</div>
+				</>
+			) : (
+				// Tare Hecha
+				<>
+					<div className="col-7 col-sm-8 col-md-8 col-lg-10 col-xl-10 myTask">
+						{props.taskText}
+						<hr></hr>
+					</div>
+
+					<div className="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+						<div className="d-flex pt-2">
+							<i
+								className="far fa-trash-alt mr-4"
+								// onClick={() =>
+								// 	props.deleteTask(null, props.id)
+								// }
+							></i>
+
+							<i
+								className="fas fa-times fa-lg"
+								// onClick={() =>
+								// 	props.notDone(props.id, props.done)
+								// }
+							></i>
+						</div>
+						<hr></hr>
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
+
+Task.propTypes = {
+	taskText: PropTypes.string,
+	deleteTask: PropTypes.func,
+	id: PropTypes.number,
+	done: PropTypes.bool,
+	addTaskDone: PropTypes.func,
+	taskDoneList: PropTypes.array,
+	notDone: PropTypes.func
+};
